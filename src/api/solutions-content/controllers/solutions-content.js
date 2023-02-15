e9c91c5c31514d414e9c3d64aeb09b2f94bd200e
aercan4 <abdurrahan_ercan4@hotmail.com>
2023-02-15 12:25:10 +0300

    feat: solutions page.

---
+++ src/api/solutions-content/controllers/solutions-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * solutions-content controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::solutions-content.solutions-content');
---
+++ src/api/solutions-content/services/solutions-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * solutions-content service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::solutions-content.solutions-content');
